9366654bef49b0cf246e25496b1423228ce17628
Donovan <dsaunders5358@gmail.com>
2022-10-08 00:18:09 +0100

    Ideas added to comment of main app

--- tests/test.py
+++ tests/test.py
@@ -3,3 +3,4 @@ def strike(text):
 
 print(strike("Testing A sentence to see what happens"))
 #update test
+#notes to self, have every string end with some characters aka dn or pr and have the propram read that to decide what state the task on the list is. Could possible add a date to the list to keep track of dates so we can then persist completed task and track how many completed per month etc
